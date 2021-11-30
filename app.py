d41469ea8329de258759e0cf8f8cfffac4fa6797
Aynaan <aynaanq@gmail.com>
2021-11-30 16:15:57 +0530

    commiting

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+import uvicorn
+from pydantic import BaseModel
+from model import Preprocessing
+import pickle
+import pandas as pd
+from tensorflow import keras
+# Creating FastAPI instance
+app = FastAPI()
+ 
+# Creating class to define the request body
+# and the type hints of each attribute
+class request_body(BaseModel):
+    title=str()
+    abstract=str()
+
+
+
+@app.post('/predict')
+def predict(data : request_body):
+    title=data.title
+    abstract=data.abstract
+    pp=Preprocessing()
+    title_input = pd.Series(title)
+    title_cleaned, title_vectorized = pp.preprocess(title_input, training="title")
+
+    abstract_input = pd.Series(abstract)
+    abstract_cleaned, abstract_vectorized = pp.preprocess(abstract_input, training="abstract")
+
+    title_vectorized=title_vectorized.toarray()
+    abstract_vectorized=abstract_vectorized.toarray()
+
+    model = keras.models.load_model("my_model.h5",compile=False)
+    y_probs=model.predict({"title": title_vectorized, "abstract": abstract_vectorized})
+    y_dict={"ComputerScience":y_probs[0][:].flatten(),"Physics":y_probs[1][:].flatten(),'Mathematics':y_probs[2][:].flatten(),'Statistics':y_probs[3][:].flatten(),'QuantitativeBiology':y_probs[4][:].flatten(),'QuantitativeFinance':y_probs[5][:].flatten()}
+    categories=['ComputerScience', 'Physics', 'Mathematics', 'Statistics',  'QuantitativeBiology', 'QuantitativeFinance']
+    threshold=[0.4666402041912079,0.41269561648368835,0.41028860211372375,0.40249261260032654,0.21160995960235596,0.1636740267276764]
+    prediction=[]
+    for (i,j) in zip(categories,threshold):
+       temp= y_dict[i][0]
+       if(temp>=j):
+           prediction.append(i)
+
+    return prediction
---
+++ model.py
@@ -0,0 +1,74 @@
+import nltk
+import pandas as pd
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import re
+import pickle
+tfidf_abstract = pickle.load(open("tfidf_abstract.pkl", 'rb'))
+tfidf_title=pickle.load(open("tfidf_title.pkl", 'rb'))
+from tensorflow import keras
+
+
+
+
+
+class Preprocessing:
+    '''Preprocesses the given column and returns data that is ready to feed to the model'''
+    
+    def __init__(self):
+        # initializing objects for different preprocessing techniques
+        self.stop_words = stopwords.words('english')
+        self.lemmatizer = WordNetLemmatizer()
+        self.vectorizer_abstract = tfidf_abstract
+        self.vectorizer_title = tfidf_title
+        self.training = None
+        
+    
+
+    def remove_punctuations(self, text):
+        # removes unnecessary punctuations from the text
+        text = text.lower()
+        cleaned_text = re.findall("[a-zA-Z]+", text)
+        
+        return cleaned_text
+    
+
+    def stop_words_remover(self, text):
+        # removes stopwords since they are not useful for sentiment prediction
+        cleaned_text = [w for w in text if not w in self.stop_words]
+        
+        return cleaned_text
+    
+    
+    def lemmatize(self, text):
+        # brings words to their root words 
+        cleaned_text = ' '.join([self.lemmatizer.lemmatize(i) for i in text])
+        
+        return cleaned_text
+    
+    
+    def vectorize(self, X_cleaned): 
+        # converting text to vectorized form
+        if self.training=="title":
+            X_vectorized = self.vectorizer_title.transform(X_cleaned)
+        else:
+            X_vectorized = self.vectorizer_abstract.transform(X_cleaned)
+                
+        return X_vectorized
+
+
+
+    
+    def preprocess(self, X, train_labels=None, training=None):
+        # takes input column and applies different pre-processing techniques
+        X_cleaned = pd.DataFrame()
+        self.training = training
+        X = X.apply(lambda x: self.remove_punctuations(x))
+        X = X.apply(lambda x: self.stop_words_remover(x))
+        X_cleaned = X.apply(lambda x: self.lemmatize(x))
+        X_vectorized=self.vectorize(X_cleaned)
+        
+        return X_cleaned, X_vectorized
+
+
